fa04cf4bd024fb531aca238c6f0643d8230156ff
Remi HOEPPE <remihoeppe@users.noreply.github.com>
2023-02-22 11:54:11 +1100

    FIX: REMOVES node-module form parent directory

--- backend/src/main/java/com/employeeCreator/app/employee/EmployeeConfig.java
+++ backend/src/main/java/com/employeeCreator/app/employee/EmployeeConfig.java
@@ -17,8 +17,8 @@ public class EmployeeConfig {
             Employee john = new Employee(
                 "John",
                 "Bill",
-                "H.",
-                "6666666",
+                "Hope",
+                "0412223344",
                 "Bob@bill.usa",
                 "Cali",
                 LocalDate.of(1879,03,17),
@@ -31,18 +31,46 @@ public class EmployeeConfig {
             Employee will = new Employee(
                     "Will",
                     "Wild",
-                    "H.",
-                    "00000",
+                    "Macca",
+                    "0499998888",
                     "Will@bill.usa",
-                    "Cali",
+                    "45 Boulter Close, Vasa Views, QLD",
                     LocalDate.of(2000,03,17),
                     LocalDate.of(2005,06,30),
                     "Permanent",
-                    "Full time",
+                    "Part time",
                     "25"
             );
 
-            employeeRepository.saveAll(List.of(john, will));
+            Employee maddie = new Employee(
+                    "Maddie",
+                    "Helen",
+                    "Jones",
+                    "0477778888",
+                    "maddie@jones.usa",
+                    "31 Glen William Road, Einasleigh, QLD",
+                    LocalDate.of(2010,03,17),
+                    LocalDate.of(2018,06,30),
+                    "Contract",
+                    "Full time",
+                    "38"
+            );
+
+            Employee rosie = new Employee(
+                    "Rosie",
+                    "Margaux",
+                    "Bloom",
+                    "0499998888",
+                    "rosie@bloom.usa",
+                    "70 Meyer Road, Cambrai, South Australia, NSW",
+                    LocalDate.of(2000,03,17),
+                    LocalDate.of(2005,06,30),
+                    "Permanent",
+                    "Part time",
+                    "25"
+            );
+
+            employeeRepository.saveAll(List.of(john, will, maddie, rosie));
 
         };
 
